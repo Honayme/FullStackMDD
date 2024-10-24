e8ab73044f10d2d5497461ccd0e476879c5a1cda
Thomas <thomas.mirabile@ynov.com>
2024-10-24 16:47:05 +0200

    Add security config

---
+++ back/src/main/java/com/openclassrooms/mddapi/configuration/SecurityConfig.java
@@ -0,0 +1,75 @@
+package com.openclassrooms.mddapi.configuration;
+
+import com.openclassrooms.mddapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * ApplicationConfig is a configuration class that sets up the application's security settings.
+ * It configures the UserDetailsService, AuthenticationProvider, AuthenticationManager, and PasswordEncoder beans.
+ */
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserRepository userRepository;
+
+    /**
+     * Configures and returns the UserDetailsService bean.
+     * It uses the UserRepository to find a user by their email.
+     *
+     * @return the configured UserDetailsService
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    /**
+     * Configures and returns the AuthenticationProvider bean.
+     * It uses the UserDetailsService and PasswordEncoder beans.
+     *
+     * @return the configured AuthenticationProvider
+     */
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    /**
+     * Configures and returns the AuthenticationManager bean.
+     * It uses the AuthenticationConfiguration to get the AuthenticationManager.
+     *
+     * @param config the AuthenticationConfiguration to use
+     * @return the configured AuthenticationManager
+     * @throws Exception if an error occurs during the configuration
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    /**
+     * Configures and returns the PasswordEncoder bean.
+     * It uses the BCryptPasswordEncoder for password encoding.
+     *
+     * @return the configured PasswordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
