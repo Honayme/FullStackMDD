8ce3f2270db5b5ce65a00effdbc6c887f4c34b81
Thomas <thomas.mirabile@ynov.com>
2024-11-01 03:33:20 +0100

    Angular - Fix errors feedback, app translated in French, expand text area for adding comment

--- front/src/app/features/auth/components/register/register.component.ts
+++ front/src/app/features/auth/components/register/register.component.ts
@@ -1,10 +1,9 @@
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, ValidationErrors, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SessionService } from 'src/app/services/session.service';
 import { AuthService } from '../../services/auth.service';
 import { RegisterRequest } from '../../interfaces/registerRequest.interface';
 import { AuthSuccess } from '../../interfaces/authSuccess.interface';
-import { User } from 'src/app/interfaces/user.interface';
 import { Component } from '@angular/core';
 
 @Component({
@@ -18,21 +17,41 @@ export class RegisterComponent {
 
   public form = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    name: ['', [Validators.required, Validators.min(3)]],
-    password: ['', [Validators.required, Validators.min(3)]]
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    password: ['', [Validators.required, Validators.minLength(8), this.passwordValidator]]
   });
-hide: any;
+  hide: any;
 
-  constructor(private authService: AuthService,
+  constructor(
+    private authService: AuthService,
     private fb: FormBuilder,
     private router: Router,
-    private sessionService: SessionService) { }
+    private sessionService: SessionService
+  ) {}
+
+  // Validateur personnalisé pour le mot de passe
+  private passwordValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.value || '';
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    const isValid =
+      hasUpperCase &&
+      hasLowerCase &&
+      hasNumber &&
+      hasSpecialChar &&
+      password.length >= 8;
+
+    return isValid ? null : { passwordInvalid: true };
+  }
 
   public submit(): void {
     const registerRequest = this.form.value as RegisterRequest;
     this.authService.register(registerRequest).subscribe({
       next: (value: AuthSuccess) => this.router.navigate(['/login']),
-      error: _ => this.onError = true,
+      error: _ => (this.onError = true),
     });
   }
 }
